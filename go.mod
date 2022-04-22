fddf55a1301f7c5a2be91592e0bbf42e7840104b
Soh Thiam Hing <daffodilistic@gmail.com>
2022-04-22 11:44:04 +0800

    Initial commit of basic Golang code and project structure

---
+++ go.mod
@@ -0,0 +1,6 @@
+module daffodilistic/carpe_vehicula
+// "Seize the Vehicles"
+
+go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"daffodilistic/carpe_vehicula/etl"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	fmt.Print(etl.Hello("daffodil"))
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
